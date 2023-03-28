743ca2e29d409c0e4ab7a4dec919a6ee2636d7de
Edwin Ichura <eduichura69@gmail.com>
2023-03-28 12:24:44 -0700

    print integer function

--- printf.c
+++ printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * _printchar - prints character
  *
@@ -14,6 +12,7 @@ void _printchar(void)
 	_putchar(va_arg(id.args, int));
 	id.count++;
 }
+
 /**
  * _printstr - prints str
  *
@@ -35,5 +34,44 @@ void _printstr(void)
 			id.j++;
 			id.count++;
 		}
+	}
+}
+
+/**
+ * _printint - prints integer
+ *
+ * Return: void
+ */
+
+void _printint(void)
+{
+	int num, div, len;
+
+	num = va_arg(id.args, int);
+	div = 1;
+	len = 0;
+	if (num < 0)
+	{
+		_putchar('-');
+		num = num * -1;
+		id.count++;
+	}
+	if (num == 0)
+	{
+		_putchar('0');
+		id.count++;
+	}
+	while (num / div > 0)
+	{
+		div = div * 10;
+		len++;
+	}
+	div = div / 10;
+	while (len > 0)
+	{
+		_putchar(((num / div) % 10) + '0');
+		div = div / 10;
+		len--;
+		id.count++;
 	}
 }
