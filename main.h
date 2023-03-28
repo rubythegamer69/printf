85ffddf9e86297e19f5a6a59532a6982704c24ce
Edwin Ichura <eduichura69@gmail.com>
2023-03-28 11:49:47 -0700

    Main .h

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct global
+{
+	char *format;
+	int i;
+	int j;
+	int count;
+	va_list args;
+	char *str;
+} id;
+
+int _printf(const char *format, ...);
+/* add putchar here */
+void _printint(void);
+void _printchar(void);
+void _printstring(void);
+
+#endif /* MAIN_H */
