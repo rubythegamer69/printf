3231798d0aee1114be66ebdc0c2592fe28546e9e
angela <angelngina98@gmail.com>
2023-03-28 12:16:59 -0700

    prints str

--- printf.c
+++ printf.c
@@ -14,3 +14,26 @@ void _printchar(void)
 	_putchar(va_arg(id.args, int));
 	id.count++;
 }
+/**
+ * _printstr - prints str
+ *
+ * Return: Nothing
+ *
+ */
+
+void _printstr(void)
+{
+	id.str = va_arg(id.args, char *);
+	if (id.str == NULL)
+		id.str = "(null)";
+	else
+	{
+		id.j = 0;
+		while (id.str[id.j] != '\0')
+		{
+			_putchar(id.str[id.j]);
+			id.j++;
+			id.count++;
+		}
+	}
+}
